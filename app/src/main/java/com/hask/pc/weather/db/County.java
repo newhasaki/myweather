c44c9f019de6511c777e6f993edf07e9c0d969f2
newhasaki <newhasaki@users.noreply.github.com>
2019-03-12 11:09:34 +0800

    加入创建数据库和表的各项配置.

---
+++ app/src/main/java/com/hask/pc/weather/db/County.java
@@ -0,0 +1,43 @@
+package com.hask.pc.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+public class County extends DataSupport {
+
+    private int id;
+    private String countyName;
+    private String watherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWatherId() {
+        return watherId;
+    }
+
+    public void setWatherId(String watherId) {
+        this.watherId = watherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
